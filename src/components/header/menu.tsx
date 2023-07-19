55f1d437f0ae9a66397f63ebbed0f4df194884c3
Bruno Lima <ibrunoliimas@gmail.com>
2023-07-19 02:30:50 -0300

    Adicionando animação abrir/fechar no Menu (mobile)

--- src/components/header/menu.tsx
+++ src/components/header/menu.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 import { links } from './links';
 import { IoMdClose } from 'react-icons/io';
 import styles from './styles.module.css';
@@ -13,33 +14,45 @@ type MenuProps = {
 
 
 export const Menu = ({ onClose }: MenuProps) => {
+    const [animate, setAnimate] = useState(false);
+    const [closing, setClosing] = useState(false);
     const pathname = usePathname();
 
-    const handleLinkClick = () => {
-        onClose();
+    useEffect(() => {
+        setAnimate(true);
+    }, []);
+
+    const handleCloseClick = () => {
+        setClosing(true);
+        setTimeout(() => {
+            onClose();
+            setClosing(false);
+        }, 500);
+    };
+
+    const getMenuLinkClass = (linkPath: string) => {
+        return `ml-8 relative py-4 text-white tracking-wider uppercase font-barlow-condensed hover:border-r-4 transition-all duration-500 ${pathname === linkPath ? 'border-r-4 border-white' : 'border-gray-300'}`;
     };
 
     return (
         <nav
-            className={`md:hidden ${styles.nav} w-[80%] bg-gray-sky bg-opacity-20 h-screen absolute -top-10 right-0 z-50`}
+            className={`md:hidden ${styles.nav} w-[80%] bg-gray-sky bg-opacity-20 h-screen absolute -top-10 right-0 z-50 ${animate ? (closing ? 'transition-transform duration-500 translate-x-full' : 'transition-transform duration-500 translate-x-0') : 'translate-x-full'
+                }`}
         >
             <div className='mt-28 flex flex-col justify-center'>
                 {links.map((link, index) => (
                     <Link
                         key={index}
                         href={link.path}
-                        className={`ml-8 relative py-4 text-white tracking-wider uppercase font-barlow-condensed hover:border-r-4 transition-all duration-500 ${pathname === link.path
-                            ? "border-r-4 border-white"
-                            : "border-gray-300"
-                            }`}
-                        onClick={handleLinkClick}
+                        className={getMenuLinkClass(link.path)}
+                        onClick={handleCloseClick}
                     >
                         <span className="mr-2 font-bold">0{index}</span>
                         {link.name}
                     </Link>
                 ))}
             </div>
-            <button className="absolute top-12 right-6 text-white" onClick={onClose}>
+            <button className="absolute top-12 right-6 text-white" onClick={handleCloseClick}>
                 <IoMdClose size={34} />
             </button>
         </nav>
